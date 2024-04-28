ba4988b7b3f83e7bf94167f51153cf61bed33eef
I, yoonah <qwer120542@gmail.com>
2024-04-28 19:22:46 +0900

    26766 , 10171

---
+++ 2024/04/240428-c/26766.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    while (N--) {
+        printf(" @@@   @@@ \n");
+        printf("@   @ @   @\n");
+        printf("@    @    @\n");
+        printf("@         @\n");
+        printf(" @       @ \n");
+        printf("  @     @  \n");
+        printf("   @   @   \n");
+        printf("    @ @    \n");
+        printf("     @     \n");
+    }
+
+    return 0;
+}
